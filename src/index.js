a6271b096b08c83bec9968377365f878a7a575c4
Halil Bilgil <hbilgil@gmail.com>
2022-07-22 23:49:09 +0300

    Edit index.js to declare imports from modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './style.css'
+/*-----Function imports-----*/
+
+import './style.css';
+import { initAddEventListeners } from './modules/UI'
+import { saveAndRender } from './modules/Storage'
+import { openFirstProject } from './modules/Project'
