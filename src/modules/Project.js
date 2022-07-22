c4a221ebbf6bef0c3f85e9723e8335cb06c31a49
Halil Bilgil <hbilgil@gmail.com>
2022-07-23 00:00:55 +0300

    Edit Project.js to declare Project create function

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -4,6 +4,18 @@
 import { lists, selectedListId } from './Storage'
 import { renderTasks, colorTasks, updateTaskCounter } from './Task'
 
+/*-----Projects Related Logic Function Declarations (4 Functions)-----*/
+
+function createProject() { //Project create function
+
+    const addProjectModalNameInput = document.querySelector('[data-new-list-input]')
+
+    return {
+        id: Date.now().toString(), //an automatically assigned id
+        name: addProjectModalNameInput.value, 
+        tasks: [] //an empty task array to be used when tasks are created
+    }
+}
 
 /*-----Function exports-----*/
 
