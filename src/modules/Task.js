7029cd19366a5df70677ecbdc97bf2a5d41dfb62
Halil Bilgil <hbilgil@gmail.com>
2022-07-22 23:51:10 +0300

    Edit Task.js to declare Task Create function

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -8,8 +8,24 @@ import Swal from 'sweetalert2'
 import { openEditTaskModal, openTaskInfoModal } from './UI'
 import { lists, selectedListId, saveAndRender } from './Storage'
 
+/*-----Task Logic Function Declarations (7 Functions)-----*/
 
+function createTask() { //Task create function
 
+    const addTaskModalNameInput = document.querySelector('[data-new-task-name-input]')
+    const addTaskModalDescriptionInput = document.querySelector('[data-new-task-detail-input]')
+    const addTaskModalDueDateInput = document.querySelector('[data-new-task-date-input]')
+    const addTaskModalPriorityInput = document.querySelector('[data-new-task-priority-input]')
+  
+    return {
+        id: Date.now().toString(), //an automatically assigned id
+        name: addTaskModalNameInput.value,
+        date: addTaskModalDueDateInput.value,
+        priority: addTaskModalPriorityInput.value,
+        description: addTaskModalDescriptionInput.value,
+        complete: false, //unchecked checkbox created initially
+    }
+}
 
 /*---Function exports---*/
 
