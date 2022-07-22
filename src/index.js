3466aa4c8879b29cdef8b735cebce9567eb23016
Halil Bilgil <hbilgil@gmail.com>
2022-07-23 00:08:43 +0300

    Edit index.js to declare add event listeners to load homepage and initiate event listeners

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import './style.css';
 import { initAddEventListeners } from './modules/UI'
 import { saveAndRender } from './modules/Storage'
 import { openFirstProject } from './modules/Project'
+
+/*-----Homepage onload add event listeners-----*/
+
+document.addEventListener('DOMContentLoaded', initAddEventListeners) //UI event listeners are imported to load homepage
+document.addEventListener('DOMContentLoaded', saveAndRender)
+document.addEventListener('DOMContentLoaded', openFirstProject) //when page refreshed or entered the first time by the user, 1st project's tasks are opened
