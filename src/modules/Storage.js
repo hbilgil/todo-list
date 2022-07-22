50be6a922c59a3fd5923596189cf90faa551e2dc
Halil Bilgil <hbilgil@gmail.com>
2022-07-22 23:48:10 +0300

    Edit Storage.js to declare import and exports from-to modules

---
+++ src/modules/Storage.js
@@ -0,0 +1,9 @@
+
+/*-----Function and Element imports-----*/
+
+import { renderProjectList } from './Project'
+
+
+/*-----Function and Element exports-----*/
+
+export { lists, selectedListId, saveAndRender }
