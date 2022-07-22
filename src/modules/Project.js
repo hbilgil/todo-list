4c33a9473722bdf111bb4237f869a4a7b0d867b3
Halil Bilgil <hbilgil@gmail.com>
2022-07-23 00:03:33 +0300

    Edit Project.js to declare a function showing user the Project's tasks and their infos

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -34,6 +34,48 @@ function renderNewProject() { //creating and appending project cards at DOM Tree
     })
 }
 
+function renderProjectList() { //Projects and their tasks will be kept, displayed and rendered into the page
+
+    const newListContainer = document.querySelector('[data-lists]');
+
+    clearElement(newListContainer)
+    renderNewProject()
+
+    const projectDisplayContainer = document.querySelector('[data-list-display-container]')
+    const selectedList = lists.find((list) => list.id === selectedListId)
+    const taskList = document.querySelector('[data-tasks]')
+    const projectTitle = document.querySelector('[data-list-title]')
+    const projectCounter = document.querySelector('[data-list-counter]')
+
+    if (!newListContainer.hasChildNodes()) { //checks if the project container has at least 1 child element
+
+        projectDisplayContainer.style.display = ''
+        taskList.style.display ='none'
+        projectTitle.innerText ='Please create a project'
+        projectCounter.innerText = ''
+    } else {
+        
+        if(selectedList === undefined) { //if there is at least 1 project in container, but NONE of them is selected
+        
+        projectDisplayContainer.style.display = ''
+        taskList.style.display ='none'
+        projectTitle.innerText ='Please choose a project'
+        projectCounter.innerText = ''
+
+        } else {
+
+        projectDisplayContainer.style.display = ''
+        projectTitle.innerHTML = `<i class="fas fa-tasks"></i> ${selectedList.name}`; //the project name will be shown as a Tasks list header
+        taskList.style.display = ''
+        updateTaskCounter(selectedList) //a function to show total # of "Uncompleted tasks"
+        clearElement(taskList) //a function to keep task container updated
+        renderTasks(selectedList) //tasks will be added into the DOM
+        colorTasks(selectedList); //tasks will have different colors based on their "priorities" (red for high, yellow for moderate and green for low priorities)
+
+        }
+    }
+}
+
 /*-----Function exports-----*/
 
 export { createProject, renderProjectList }
