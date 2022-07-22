0d19dad179414a87349309e738c94a8248fb45df
Halil Bilgil <hbilgil@gmail.com>
2022-07-22 23:52:41 +0300

    Edit Task.js to declare Task Render function to add Task cards into DOM

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -27,6 +27,55 @@ function createTask() { //Task create function
     }
 }
 
+function renderTasks(selectedList) { //creating and appending task cards at DOM Tree
+
+    const taskList = document.querySelector('[data-tasks]')
+
+    selectedList.tasks.forEach((task) => {
+
+        const selectedList = lists.find((list) => list.id === selectedListId)
+
+        const taskCardTemplate = document.getElementById('task-card-template')
+
+        const taskCard = document.importNode(taskCardTemplate.content, true) //task card elements will be given by a predefined template in HTML document
+        //task.id will be assigned to both checkbox and label element to be used when deciding whether it is completed or NOT
+        const checkBox = taskCard.querySelector('input')
+        checkBox.id = task.id
+        checkBox.checked = task.complete
+
+        const label = taskCard.querySelector('label')
+        label.htmlFor = task.id
+        
+        const lineBreak = document.createElement("br"); //a lineBreak element is created for a better styling of infos
+        label.append(task.name, lineBreak, task.date); //task name and due date is shown initially as the other infos are shown by a task info modal
+
+        //new buttons are created in every task card one by one for editing, showing other infos and deleting the tasks individually
+        
+        const taskId = task.id
+
+        const editTaskButton = document.createElement("p");
+        editTaskButton.innerHTML = `<i class="fa-solid fa-pen-to-square" aria-hidden="true"></i>`;
+        editTaskButton.addEventListener("click", () => openEditTaskModal(task, taskId));
+
+        const taskInfoButton = document.createElement('p');
+        taskInfoButton.innerHTML = `<i class="fa-solid fa-up-right-and-down-left-from-center"></i>`;
+        taskInfoButton.addEventListener("click", () => enlargeTask(task, selectedList));
+
+        const taskName = task.name // taskName variable is created to be used in deleteTask function separately
+
+        const deleteTaskButton = document.createElement('p');
+        deleteTaskButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+        deleteTaskButton.addEventListener("click", () => deleteTask(taskName, selectedList));
+
+        const flexTwo = taskCard.querySelector(".flex-2");
+        flexTwo.append(editTaskButton);
+        flexTwo.append(taskInfoButton);
+        flexTwo.append(deleteTaskButton);
+
+        taskList.appendChild(taskCard)
+    })
+}
+
 /*---Function exports---*/
 
 export { createTask, renderTasks, colorTasks, searchTask, updateTaskCounter }
