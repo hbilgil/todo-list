4de81a510db9e62ccc1293fd3ef5348a3b7ed354
Halil Bilgil <hbilgil@gmail.com>
2022-07-22 23:48:32 +0300

    Edit Project.js to declare import and exports from-to modules

---
+++ src/modules/Project.js
@@ -0,0 +1,10 @@
+
+/*-----Function and element imports-----*/
+
+import { lists, selectedListId } from './Storage'
+import { renderTasks, colorTasks, updateTaskCounter } from './Task'
+
+
+/*-----Function exports-----*/
+
+export { createProject, renderProjectList }
